5224f64a4755f513aff55d54b70d20277ab51e2a
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-14 21:47:44 +0700

    Initial commit

---
+++ src/main/java/com/alvin/compfest/CompfestApplication.java
@@ -0,0 +1,12 @@
+package com.alvin.compfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompfestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alvin/compfest/CompfestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvin.compfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
