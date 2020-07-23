e2a32c38a59432a06ebaa01d1f909e0701ae6006
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-23 20:23:42 +0700

    Allow all origins

--- src/main/java/com/alvin/compfest/CompfestApplication.java
+++ src/main/java/com/alvin/compfest/CompfestApplication.java
@@ -2,11 +2,24 @@ package com.alvin.compfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CompfestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompfestApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
