deb5e7dfaa966d41fd01f4cc1a552aea898200dc
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-24 20:30:03 +0700

    Encrypt user password

--- src/main/java/com/alvin/compfest/user/UserService.java
+++ src/main/java/com/alvin/compfest/user/UserService.java
@@ -4,18 +4,22 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserDAO userDAO;
+    private final BCryptPasswordEncoder bcryptPasswordEncoder;
 
     @Autowired
-    public UserService(@Qualifier("arrayListUserDAO") UserDAO userDAO) {
+    public UserService(@Qualifier("arrayListUserDAO") UserDAO userDAO, BCryptPasswordEncoder bcryptPasswordEncoder) {
         this.userDAO = userDAO;
+        this.bcryptPasswordEncoder = bcryptPasswordEncoder;
     }
 
     public User createUser(User user) {
+        user.setPassword(bcryptPasswordEncoder.encode(user.getPassword()));
         return this.userDAO.insertUser(user);
     }
 
