968de990337170f05bea35c0e022affa7121bb81
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-27 14:54:18 +0700

    Fix cors error on put method

--- src/main/java/com/alvin/compfest/CompfestApplication.java
+++ src/main/java/com/alvin/compfest/CompfestApplication.java
@@ -19,7 +19,7 @@ public class CompfestApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 			}
 		};
 	}
